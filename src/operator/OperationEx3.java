afeb535320690cdd46efb82fd4c70ee71ed536dd
codedbyminjae <minjaekim7111@naver.com>
2025-10-14 00:15:38 +0900

    operation

---
+++ src/operator/OperationEx3.java
@@ -0,0 +1,11 @@
+package operator;
+
+public class OperationEx3 {
+    public static void main (String[] args) {
+        int score = 120;
+
+        boolean result = score>= 80 && score<=100 ? true : false;
+
+        System.out.println(result);
+    }
+}
