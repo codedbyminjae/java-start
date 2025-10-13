27257f073b1eb9b7473a0d78a7cb17b44a53d526
codedbyminjae <minjaekim7111@naver.com>
2025-10-13 23:59:17 +0900

    variable

---
+++ src/variable/VarEx3.java
@@ -0,0 +1,12 @@
+package variable;
+
+public class VarEx3 {
+
+    public static void main(String[] args) {
+        long longVar = 1000000000;
+        System.out.println(longVar);
+
+        boolean booleanVar = true;
+        System.out.println(booleanVar);
+    }
+}
